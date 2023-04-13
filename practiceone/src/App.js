86f58ad4ce82287558ddd84cd967df3f109bb6ba
aginga <brianaginga35@gmail.com>
2023-04-13 08:11:29 -0700

    react practice

---
+++ practiceone/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Bar from './Bar';
+import Home from './Home';
+
+
+
+function App() {
+
+  // const birth = 'Tomorrow is Eunita Birthday';
+  // const link = 'https://www.google.com';
+  return (
+    <div className="App">
+      <Bar />
+        <div className="content">
+          <Home />
+        </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ practiceone/src/Bar.js
@@ -0,0 +1,11 @@
+const Bar = () => {
+    return (  
+        <div className="bar">
+        <h2>The Creative Work</h2>
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+    );
+}
+ 
+export default Bar;
